65fd2e379841044f4abe71bfaf5aeb188db9dd8f
Rachel <gggggrea@gmail.com>
2023-03-27 21:52:12 +0100

    Create components to create and show books

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
 function App() {
-  return <div> App </div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("need to add book with: ", title);
+    const updatedBooks = [...books, { id: Math.trunc(Math.random() * 9999), title }];
+    setBooks(updatedBooks);
+    console.log(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <BookList books={books} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
