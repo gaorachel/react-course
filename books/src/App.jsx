9a308c7f351b5ad86cd2f4632ec036dc617852f0
Rachel <gggggrea@gmail.com>
2023-03-28 18:21:49 +0100

    Connect the local API with the APP

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,31 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
+  const port = "http://localhost:3001/books";
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get(port);
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`${port}/${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { id: book.id, title: newTitle };
-      } else return book;
+        return { ...book, ...response.data };
+      }
+      return book;
     });
+    // const updatedBooks = books.map((book) => {
+    //   if (book.id === id) {
+    //     return { id: book.id, title: newTitle };
+    //   } else return book;
+    // });
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`${port}/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    console.log("need to add book with: ", title);
-    const updatedBooks = [...books, { id: Math.trunc(Math.random() * 9999), title }];
-    setBooks(updatedBooks);
+  const createBook = async (title) => {
+    const response = await axios.post(port, { title });
+    const updatedBooks = [...books, response.data];
     console.log(updatedBooks);
+
+    // console.log("need to add book with: ", title);
+    // const updatedBooks = [...books, { id: Math.trunc(Math.random() * 9999), title }];
+    // setBooks(updatedBooks);
+    // console.log(updatedBooks);
   };
 
   return (
