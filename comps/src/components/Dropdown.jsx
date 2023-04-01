162a35e11ec35aa104953fc43f3457a2745b6033
Rachel <gggggrea@gmail.com>
2023-04-01 09:41:27 +0100

    [WIP] create a dropdown component

---
+++ comps/src/components/Dropdown.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function Dropdown({ options, selection, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (selectedOption) => {
+    setIsOpen(false);
+    onSelect(selectedOption);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  //   let content = "Select...";
+  //   if (selection) {
+  //     content = selection.label;
+  //   }
+
+  return (
+    <div>
+      <div onClick={handleClick}>{selection?.label || "Select..."}</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
+}
+
+export default Dropdown;
